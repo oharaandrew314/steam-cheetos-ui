2557ca27512f6b33eff35f22ccf4dd4568771071
aohara-cc <aohara@camcloud.com>
2022-04-02 14:21:59 -0400

    initial commit

---
+++ android/app/src/main/kotlin/dev/andrewohara/steamcheetos_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package dev.andrewohara.steamcheetos_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/views/screens/LoginScreen.dart
@@ -0,0 +1,12 @@
+
+import 'package:flutter/widgets.dart';
+
+class LoginScreen extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+}
